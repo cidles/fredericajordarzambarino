b0ce3005660146b53857c2817642b975a000b09d
joaoseguro <seguro_joao@hotmail.com>
2014-06-03 12:26:09 +0100

    Pap

--- www/js/index.js
+++ www/js/index.js
@@ -1,64 +1,182 @@
 var app = {
-    // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        reset_values();
+        loadjson();
     },
-    // Update DOM on a Received Event
     receivedEvent: function(id) {
-        /*var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-        */
-        console.log('Received Event: ' + id);
-        load()
+        //console.log('Received Event: ' + id);
     }
 };
 
-function load() {
-var json = (function () {
-    var json = null;
-    $.ajax({
-        'async': false,
-        'global': false,
-        'url': my_url,
-        'dataType': "json",
-        'success': function (data) {
-            json = data;
-        }
-    });
-    return json;
-})(); 
+var current_question_number = 0;
+var money_won = "0 €";
+var perguntas_usadas = [0];
+var json = [];
+var randomnumber = 0;
+var answered =  false;
+
+function reset_values() {
+    current_question_number = 0;
+    money_won = 0;
+    perguntas_usadas = [0];
+    andomnumber = 0;
+    answered =  false;
+
 }
 
-function reset() {
-
+function loadjson() {
+    json = (function () {
+        var json = null;
+        $.ajax({
+            'async': false,
+            'global': false,
+            'url': 'perguntas.json',
+            'dataType': "json",
+            'success': function (data) {
+                json = data;
+            }
+        });
+        return json;
+    })(); 
 }
 
 function start_new_game() {
     $("#start").css( "display", "none" );
-    $("#money").css( "display", "inline" );
-    //change color
-    setTimeout(function() {start_new_game2()}, 2000);
+
+    //Clear money colors
+    for (var i = 1; i <= 15; i++) { 
+        $("#money" + i).css("color", "black");
+    }
+
+    //Clear ajudas
+    $("#call").prop('disabled', false);
+    $("#publico").prop('disabled', false);
+    $("#fiftyfifty").prop('disabled', false);
+
+    next_question();
 }
 
-function start_new_game2() {
+function next_question() {
+    answered = false;
+
+    $("#game").css( "display", "none" );
+    if (current_question_number != 0) {
+        $("#money" + current_question_number).css("color", "green");
+        //Update money won
+        money_won = $("#money" + current_question_number + " .col-xs-9").html();
+    }
+    current_question_number++;
+    $("#money" + current_question_number).css("color", "orange");
+    $("#money").css( "display", "inline" );
+    setTimeout(function() {back_to_game()}, 2800);
+
+    //Clear answers
+    for (var i = 1; i <= 4; i++) {
+        $("#answer" + i).html("");
+        $("#answer" + i).prop('disabled', true);
+        $("#answer" + i).addClass('btn-warning')
+                        .removeClass('btn-info')
+                        .removeClass('btn-success')
+                        .removeClass('btn-danger'); 
+    };
+
+    //load question
+    randomnumber = Math.floor(Math.random()*(json.length))
+    while ($.inArray(randomnumber, perguntas_usadas) != -1) {
+        randomnumber = Math.floor(Math.random()*(json.length))
+    }
+
+    perguntas_usadas.push(randomnumber); // NOOOOOOOT WORKING I think
+
+    if (json[randomnumber][4] != "") {
+        $("#picture").attr("src", "img/" + json[randomnumber][4]);
+    }
+    else $("#picture").attr("src", "img/question_mark.png");
+
+    $("#question").html(json[randomnumber][1]);
+
+    for (var i = 1; i <= json[randomnumber][2].length; i++) { 
+        $("#answer" + i).html(json[randomnumber][2][i - 1]);
+        $("#answer" + i).prop('disabled', false);
+    };
+}
+
+function responder(resposta_n) {
+    if (!answered){
+        answered = true;
+
+        $("#answer" + resposta_n).addClass('btn-info').removeClass('btn-warning');
+        if (resposta_n ==  json[randomnumber][3]) {
+            setTimeout(function() {
+                $("#answer" + resposta_n).addClass('btn-success').removeClass('btn-info');
+            }, 1500);
+            setTimeout(function() {next_question()}, 3000);
+        }
+        else {
+            setTimeout(function() {
+                $("#answer" + resposta_n).addClass('btn-danger').removeClass('btn-info');
+            }, 1500);
+            setTimeout(function() {
+                $("#answer" + json[randomnumber][3]).addClass('btn-success').removeClass('btn-warning');
+            }, 1500);
+            setTimeout(function() {game_over()}, 5500);
+        }
+    }
+}
+
+function back_to_game() {
     $("#money").css( "display", "none" );
     $("#game").css( "display", "inline" );
+}
+
+function home() {
+    reset_values();
+    $("#money").css( "display", "none" );
+    $("#game").css( "display", "none" );
+    $("#game_over").css( "display", "none" );
+    $("#start").css( "display", "inline" );
+}
+
+function game_over() {
+    $("#money_won").html(money_won);
+    reset_values();
+    $("#money").css( "display", "none" );
+    $("#game").css( "display", "none" );
+    $("#start").css( "display", "none" );
+    $("#game_over").css( "display", "inline" );
+
+}
+
+function call(){
+$("#call").prop('disabled', true);
+}
+
+function publico(){
+$("#publico").prop('disabled', true);
+}
+
+function fiftyfifty(){
+    removed = 1;
+    toremove = 0;
+
+    if (json[randomnumber][2].length <= 3) {
+        toremove = 1;
+    }
+    else {
+        toremove = 2;
+    }
+    for (i = 1; i <= 4; i++) {
+        if (i != json[randomnumber][3] && removed <= toremove) {
+            $("#answer" + i).prop('disabled', true);
+            removed++;
+        }
+    }
+    $("#fiftyfifty").prop('disabled', true);
 }
